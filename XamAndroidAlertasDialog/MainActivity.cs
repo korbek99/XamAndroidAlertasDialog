a4331462e589796fcdd636b16259e7e8a01c31df
korbek100 <korbek100@gmail.com>
2018-05-07 19:25:21 -0300

    first commit

---
+++ XamAndroidAlertasDialog/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System;
+
+namespace XamAndroidAlertasDialog
+{
+    [Activity(Label = "XamAndroidAlertasDialog", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+
+        public override void OnBackPressed()
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.SetMessage("Desea Salir de la Aplicacion?");
+            builder.SetTitle("Salir");
+            builder.SetPositiveButton("Si",PositiveButton);
+            builder.SetNegativeButton("No", NegativeButton);
+            //base.OnBackPressed();
+            AlertDialog dialog = builder.Create();
+            dialog.Show();
+        }
+
+        private void NegativeButton(object sender, DialogClickEventArgs e)
+        {
+           // throw new NotImplementedException();
+        }
+
+        private void PositiveButton(object sender, DialogClickEventArgs e)
+        {
+            // throw new NotImplementedException();
+            Finish();
+        }
+    }
+}
+
